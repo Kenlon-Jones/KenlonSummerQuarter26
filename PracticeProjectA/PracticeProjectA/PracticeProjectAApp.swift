1dd974370d79a8dd5e436c6428dd5ce19cfb9421
Kenlon-Jones <kenlon.jones@hgs.hiddengeniusproject.org>
2026-06-15 14:29:29 -0700

    Uploaded Practice Project A
    - Created an empty Xcode project

---
+++ PracticeProjectA/PracticeProjectA/PracticeProjectAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProjectAApp.swift
+//  PracticeProjectA
+//
+//  Created by Kenlon Jamone Jones Jr. on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProjectAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
